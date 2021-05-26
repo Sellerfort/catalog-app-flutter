d94e2bd787aebbcea14826422adf1521512f106e
Ruksar Ahmed <ruksar.org@gmail.com>
2021-05-26 23:14:31 +0530

    Day 2 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sellerfort/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,14 +8,9 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to flutter"),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
